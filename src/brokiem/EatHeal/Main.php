960563bed702b6091c0ba01a66764d2eec96080e
brokiem <61258338+brokiem@users.noreply.github.com>
2020-11-07 15:21:56 +0800

    Fix

--- src/brokiem/EatHeal/Main.php
+++ src/brokiem/EatHeal/Main.php
@@ -4,20 +4,27 @@ namespace brokiem\EatHeal;
 
 use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;
-use pocketmine\event\PlayerItemConsumeEvent;
+use pocketmine\event\entity\EntityRegainHealthEvent;
+use pocketmine\event\player\PlayerItemConsumeEvent;
 use pocketmine\utils\Config;
 
 class Main extends PluginBase implements Listener {
 
   public function onEnable() {
     $this->getServer()->getPluginManager()->registerEvents($this, $this);
-    $cfg = new Config($this->getDataFolder() . "config.yml", Config::YAML, array(
+    $this->cfg = new Config($this->getDataFolder() . "config.yml", Config::YAML, array(
       "healing" => 3
     ));
   }
 
+  public function onHeal(EntityRegainHealthEvent $event){
+    if($event->getRegainReason() === EntityRegainHealthEvent::CAUSE_SATURATION){
+      $event->setCancelled();
+    }
+  }
+
   public function onConsume(PlayerItemConsumeEvent $event){
     $player = $event->getPlayer();
-    $player->setHealth($player->getHealth() + (int) $cfg->get("healing") ? 3);
+    $player->setHealth($player->getHealth() + (int) $this->cfg->get("healing"));
   }
 }
